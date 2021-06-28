9c6bc5777eebf73fc415576fbbe64b5cceeda856
Timothy M Herron <tmherron09@gmail.com>
2021-06-28 15:33:01 -0500

    Added VendorResult -Stores individual result from Book ReSeller Web Scrape if any interested vendors exsist. -Added GetLinkToVendor method based on site button values. -Added ToString() method.

---
+++ BookResellerWebScraper/VendorResult.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookResellerWebScraper
+{
+    public class VendorResult
+    {
+        public BookInfo Book { get; private set; }
+        public string VendorId { get; set; }
+        public string VendorName { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public string Link { get => GetLinkToVendor(); }
+
+
+        public VendorResult()
+        {
+
+        }
+        public VendorResult(BookInfo book, string vendorId, string vendorName, decimal price)
+        {
+            Book = book;
+            VendorId = vendorId;
+            VendorName = vendorName;
+            PurchasePrice = price;
+        }
+        public VendorResult(BookInfo book, string vendorId, string vendorName, string price)
+        {
+            Book = book;
+            VendorId = vendorId;
+            VendorName = vendorName;
+            PurchasePrice = decimal.Parse(price);
+        }
+        public VendorResult(BookInfo book, string[] details)
+        {
+            Book = book;
+            VendorId = details[0];
+            VendorName = details[1];
+            PurchasePrice = decimal.Parse(details[2]);
+        }
+
+        public string GetLinkToVendor() => $"https://api.bookscouter.com/exits/sell/{VendorId}/{Book.ISBN}";
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendJoin(' ', new string[] { "Vendor Id:", VendorId, "Vendor Name:", VendorName, "|| Purchasing for", PurchasePrice.ToString("{0:C}") });
+            return sb.ToString();
+        }
+
+    }
+}
